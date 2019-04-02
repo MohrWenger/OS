0daf478a39a964caf4a7ccb7b0d4c22947c90592
ellonet <ellonet16@gmail.com>
2019-04-02 21:06:38 +0300

    bla bla

--- ex2/cmake-build-debug/CMakeFiles/ex2.dir/cmake_clean.cmake
+++ ex2/cmake-build-debug/CMakeFiles/ex2.dir/cmake_clean.cmake
@@ -1,11 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/ex2.dir/main.cpp.o"
-  "CMakeFiles/ex2.dir/uthreads.cpp.o"
+  "CMakeFiles/ex2.dir/demo_itimer.c.o"
   "ex2.pdb"
   "ex2"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
