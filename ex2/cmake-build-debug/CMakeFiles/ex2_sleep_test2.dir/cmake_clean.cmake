2c00e4af590fc185108312b41ed99fce80f76043
ellonet <ellonet16@gmail.com>
2019-04-14 20:42:00 +0300

    passed sleep test 1

---
+++ ex2/cmake-build-debug/CMakeFiles/ex2_sleep_test2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2_sleep_test2.dir/uthreads.cpp.o"
+  "CMakeFiles/ex2_sleep_test2.dir/sleeping_threads_list.cpp.o"
+  "CMakeFiles/ex2_sleep_test2.dir/SleepTest2.cpp.o"
+  "ex2_sleep_test2.pdb"
+  "ex2_sleep_test2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex2_sleep_test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
