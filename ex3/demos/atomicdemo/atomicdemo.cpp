6a67993ed9b21989edaef540de6b8835416a9c05
mohr <mohr.wenger669@gmail.com>
2019-04-28 13:25:30 +0300

    startinggg

--- ex3/demos/atomicdemo/atomicdemo.cpp
+++ ex3/demos/atomicdemo/atomicdemo.cpp
@@ -17,19 +17,21 @@ void* foo(void* arg)
     for (int i = 0; i < 1000; ++i) {
         // old_value isn't used in this example, but will be necessary
         // in the exercise
+
         int old_value = (*(tc->atomic_counter))++;
+//        printf ("old = %d \n",old_value);
         (*(tc->bad_counter))++;
     }
-    
+    printf ("heree\n");
     return 0;
 }
 
 
 int main(int argc, char** argv)
 {
-    pthread_t threads[MT_LEVEL];
+    pthread_t threads[MT_LEVEL]; // keeps all the threads
     ThreadContext contexts[MT_LEVEL];
-    std::atomic<int> atomic_counter(0);
+    std::atomic<int> atomic_counter(0); // I think its a counter that uses atomic wy so we make sure that there are no context switch in the middle
     int bad_counter = 0;
 
     for (int i = 0; i < MT_LEVEL; ++i) {
@@ -37,11 +39,11 @@ int main(int argc, char** argv)
     }
     
     for (int i = 0; i < MT_LEVEL; ++i) {
-        pthread_create(threads + i, NULL, foo, contexts + i);
+        pthread_create(threads + i, NULL, foo, contexts + i); // create a new thread with the foo , and all the other parameters according to its num in the arrays
     }
     
     for (int i = 0; i < MT_LEVEL; ++i) {
-        pthread_join(threads[i], NULL);
+        pthread_join(threads[i], NULL); // joins the thread to make sure it runs and doesn't exit the main before
     }
     printf("atomic counter: %d\n", atomic_counter.load());
     printf("bad counter: %d\n", bad_counter);
