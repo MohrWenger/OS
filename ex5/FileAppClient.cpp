b5a4bd830df244d766d749faf8795e2d7bac0c50
Ella Duvdevan <ellonet@cs.huji.ac.il>
2019-06-19 15:16:09 +0300

    ex5

--- ex5/FileAppClient.cpp
+++ ex5/FileAppClient.cpp
@@ -20,22 +20,9 @@
 
 using namespace std;
 
-//int main(int argc, char *argv[])
-//{
-//    // FileApp -u <local_path> <remote_name> <port_no> <server_IP>
-//    // FileApp -d <local_path> <remote_name> <port_no> <server_IP>
-//    if (argc != 6)
-//    {
-//        cerr << "Bad input" << endl;
-//        return 1;
-//    }
-//
-//    struct hostent *h;
-//    if ((h = gethostbyname(argv[1])) == NULL)
-//    {
-//        fprintf(stderr, "gethostbyname ");
-//        exit(1);
-//    }
-//
-//    return 0;
-//}
+int main(int argc, char *argv[])
+{
+    // FileApp -u <local_path> <remote_name> <port_no> <server_IP>
+    // FileApp -d <local_path> <remote_name> <port_no> <server_IP>
+    return 0;
+}
--- ex5/FileAppServer.cpp
+++ ex5/FileAppServer.cpp
@@ -21,13 +21,5 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     // FileApp -s <local_dir_path> <port_no>
-    struct hostent *h;
-    if ((h = gethostbyname(argv[1])) == nullptr)
-    {
-        fprintf(stderr, "gethostbyname ");
-        exit(1);
-    }
-    cout << "Host name : " << h->h_name;
-    printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *) h->h_addr)));
     return 0;
 }
